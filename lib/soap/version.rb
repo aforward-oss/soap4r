33e41d17727b63856a57776ce3813c8b0bcc3a8f
Andrew Forward <aforward@gmail.com>
2012-01-30 13:19:10 -0500

    including the standalone server as part of soap5r

--- lib/soap/version.rb
+++ lib/soap/version.rb
@@ -1,4 +1,3 @@
-# encoding: ASCII-8BIT
 module SOAP
-  VERSION = "2.0.1" unless SOAP.const_defined?("VERSION")
+  VERSION = "2.0.2" unless SOAP.const_defined?("VERSION")
 end
--- lib/soap5r.rb
+++ lib/soap5r.rb
@@ -1,3 +1,4 @@
 
 
 require File.dirname(__FILE__) + "/soap/version"
+require File.dirname(__FILE__) + "/soap/rpc/standaloneServer"
